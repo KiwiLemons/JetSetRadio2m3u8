ad173fda679e6b27ed6591b7cb3596a07adcebc5
KiwiLemons <26863753+KiwiLemons@users.noreply.github.com>
2020-03-10 04:18:04 -0500

    Updated with new sources

--- JetSetRadio2m3u8.py
+++ JetSetRadio2m3u8.py
@@ -1,48 +1,48 @@
+# add argument to update sources
 import argparse
 import re
 import os
 import urllib.request
+import sys
 
-sources = ["https://jetsetradio.live/audioplayer/stations/classic/~list.js",
-"https://jetsetradio.live/audioplayer/stations/doomriders/~list.js",
-"https://jetsetradio.live/audioplayer/stations/future/~list.js",
-"https://jetsetradio.live/audioplayer/stations/ggs/~list.js",
-"https://jetsetradio.live/audioplayer/stations/goldenrhinos/~list.js",
-"https://jetsetradio.live/audioplayer/stations/immortals/~list.js",
-"https://jetsetradio.live/audioplayer/stations/loveshockers/~list.js",
-"https://jetsetradio.live/audioplayer/stations/noisetanks/~list.js",
-"https://jetsetradio.live/audioplayer/stations/poisonjam/~list.js",
-"https://jetsetradio.live/audioplayer/stations/rapid99/~list.js",
-"https://jetsetradio.live/audioplayer/stations/summer/~list.js"]
+sources = ["butterflies","christmas","classic","crazytaxi","doomriders",
+"elaquent","endofdays","future","ggs","goldenrhinos",
+"halloween","hover","immortals","kingforanotherday","lofi",
+"loveshockers","memoriesoftokyoto","noisetanks","ollieking","poisonjam",
+"rapid99","revolution","summer","toejamandearl","ultraremixes"]
+stationsDirectory = "radio/stations"
+
+if sys.version_info < (3, 0):
+    raise "This program requires Python version 3 or greater."
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-m","--meaning",help="Displays the type of music that each m3u8 file contains",action="store_true")
 parser.add_argument("-d","--destination", help="The dir where the m3u8 files are placed. If left blank it becomes the dir the program is running from.")
+#parser.add_argument("-u", "--update", help="Attempts to update the sources list and url format.",action="store_true")
+#parser.add_argument("-s","--skip", help="Stations to skip. Stations can be typed as short as possible while remaining identifiable and require spaces between each station. Ex: cl for classic and g for ggs")
 args = parser.parse_args()
-if args.meaning:
-     print("CLASSIC - Plays the Original Jet Set Radio Soundtrack!\nFUTURE - Plays the Jet Set Radio Future Soundtrack!\nGG's - Funky energetic beats with positive vibes!\nPOISON JAM - Grunge rock & metal with that monster sound!\nNOISE TANKS - Digital & electronic sounds to fry your brain!\nLOVE SHOCKERS - Twisted, heartbroken, & love-sick ladies!\nRAPID 99 - Upbeat & chill female tunes!\nTHE IMMORTALS - Ethnic music & balkan beats!\nDOOM RIDERS - Biker music & classic rock!\nGOLDEN RHINOS - Hip-Hop beats with deep classical rhythms of power and authority!")
+
+if args.destination:
+    dest = args.destination
 else:
-     if args.destination:
-         dest = args.destination
-     else:
-         dest = os.getcwd()
-     for source in sources:
-         #add except for no internet connection
-         response = urllib.request.urlopen(source)
-         text = response.read().decode('utf-8')
-         #findall returns a list with tuples in each element for groups
-         matches = re.findall(r"(.+)Array\[\1Array\.length\] = \"([^\"]+)\"\;",text)
-         #test if this is faster
-         station = matches[0][0].lower()
-         try:
-             newfile = open(f"{dest}\\{matches[0][0][0].upper() + matches[0][0][1:]}.m3u8","w")
-         except FileNotFoundError as error:
-             #Currently if a destination is not valid then you have to go through the entire process of evaluating a whole source before it is noticed.
-             print(f"{dest} does not exist.")
-             break
-         else:
-             newfile.write("#")
-             for idx, song in enumerate(matches):
-                 newfile.write(f"\nhttps://jetsetradio.live/audioplayer/stations/{station}/{matches[idx][1]}.mp3")
-             newfile.close()
-             print(f"Finished {matches[0][0][0].upper() + matches[0][0][1:]}")
+    dest = os.getcwd()
+    for source in sources: 
+        #add exception for no internet connection and 404
+        response = urllib.request.urlopen(f"https://jetsetradio.live/{stationsDirectory}/{source}/~list.js")  
+        text = response.read().decode('utf-8')
+        response.close()
+        #findall returns a list with tuples in each element for groups
+        tracks = re.findall(r"] = \"([^\"]+)\"\;",text)
+        try:
+            #newfile = open(f"{dest}\\{matches[0][0][0].upper() + matches[0][0][1:]}.m3u8","w")
+            newfile = open(f"{dest}\\{source}.m3u8","w")
+        except FileNotFoundError as error:
+            #Currently if a destination is not valid then you have to go through the process of evaluating a whole source before it is noticed.
+            print(f"{dest} does not exist.")
+            break
+        else:
+            newfile.write("#")
+            for song in tracks:
+                newfile.write(f"\nhttps://jetsetradio.live/{stationsDirectory}/{source}/{song}.mp3")
+            newfile.close()
+            print(f"Finished {source}")
